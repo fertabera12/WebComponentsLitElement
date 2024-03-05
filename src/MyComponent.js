5022a091627ac52a657a72e6873c4d185cc80a90
Fernanda Tabera Bazan <fernandatabera@gmail.com>
2024-03-05 14:25:18 -0600

    Render condicional

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -7,6 +7,8 @@ export class MyComponent extends LitElement {
     this.title = 'Hola Fersi';
     this.other = 'Web component';
     this.list = ['lit', 'OpenWC','LitHtml'];
+    this.isTrue = true;
+    this.age = 18;
   }
 
 
@@ -14,10 +16,23 @@ export class MyComponent extends LitElement {
   render() { //
     return html`
     <h1>${this.title}</h1>
+
     <p>esto es un ${this.other}</p>
+
     <ul>
       ${this.list.map((item) => html`<li>${item}</li>`)}
     </ul>
+
+    ${this.isTrue
+    ? html `<p>Es cierto </p>`
+    : html `<p>Es falso</p>`
+    }
+
+  ${this.age >= 18
+    ? html `<p>Es mayor </p>`
+    : html `<p>Es menor</p>`
+    }
     `;
-  }
+    }
 }
+
